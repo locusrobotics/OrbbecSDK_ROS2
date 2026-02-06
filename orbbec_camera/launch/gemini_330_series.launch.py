6321cddbe24bed3dd810bc200f34752cff8bd08d
Sushant Raj <65975293+SushiRaj99@users.noreply.github.com>
2026-02-06 11:50:18 -0500

    APG-2274 Sets Up Pick Cameras to respawn on crashes (#23)

--- orbbec_camera/launch/gemini_330_series.launch.py
+++ orbbec_camera/launch/gemini_330_series.launch.py
@@ -1,10 +1,11 @@
 import os
 import yaml
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, OpaqueFunction, GroupAction
+from launch.actions import DeclareLaunchArgument, OpaqueFunction, GroupAction, RegisterEventHandler, TimerAction
 from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import PushRosNamespace, ComposableNodeContainer, Node
+from launch_ros.actions import PushRosNamespace, ComposableNodeContainer, Node, LoadComposableNodes
 from launch_ros.descriptions import ComposableNode
+from launch.event_handlers import OnProcessStart
 
 
 def load_yaml(file_path):
@@ -70,6 +71,8 @@ def load_parameters(context, args):
 def generate_launch_description():
     args = [
         DeclareLaunchArgument('namespace', default_value=''),
+        DeclareLaunchArgument('respawn', default_value='false'),
+        DeclareLaunchArgument('respawn_delay', default_value='5.0'),
         DeclareLaunchArgument('output', default_value='screen'),
         DeclareLaunchArgument('device_type', default_value='camera'),
         DeclareLaunchArgument('camera_name', default_value='camera'),
@@ -327,6 +330,8 @@ def generate_launch_description():
         namespace = LaunchConfiguration("namespace").perform(context)
         output = LaunchConfiguration("output").perform(context)
         camera_name = LaunchConfiguration("camera_name").perform(context)
+        respawn = LaunchConfiguration("respawn").perform(context) == 'true'
+        respawn_delay = float(LaunchConfiguration("respawn_delay").perform(context))
         ros_distro = os.environ.get("ROS_DISTRO", "humble")
 
         if ros_distro == "foxy":
@@ -338,6 +343,8 @@ def generate_launch_description():
                     namespace=namespace,
                     parameters=params,
                     output=output,
+                    respawn=respawn,
+                    respawn_delay=respawn_delay,
                 )
             ]
         else:
@@ -345,22 +352,47 @@ def generate_launch_description():
             if namespace:
                 actions.append(PushRosNamespace(namespace))
 
-            actions.append(
-                ComposableNodeContainer(
+            camera_component = ComposableNode(
+                package="orbbec_camera",
+                plugin="orbbec_camera::OBCameraNodeDriver",
+                name=camera_name,
+                parameters=params,
+            )
+            
+            container = ComposableNodeContainer(
                     name=camera_name + "_container",
                     namespace="",
                     package="rclcpp_components",
                     executable="component_container",
-                    composable_node_descriptions=[
-                        ComposableNode(
-                            package="orbbec_camera",
-                            plugin="orbbec_camera::OBCameraNodeDriver",
-                            name=camera_name,
-                            parameters=params,
-                        ),
-                    ],
+                    respawn=respawn,
+                    respawn_delay=respawn_delay,
+                    composable_node_descriptions=[camera_component],
                     output=output,
+                    arguments=['--ros-args', '--log-level', 'INFO'],
                 )
+            
+            actions.append(
+                container
+            )
+
+            reload_on_restart = RegisterEventHandler(
+                OnProcessStart(
+                    target_action=container,
+                    on_start=[
+                        TimerAction(
+                            period=1.0,
+                            actions=[
+                                LoadComposableNodes(
+                                    composable_node_descriptions=[camera_component],
+                                    target_container=container,
+                                )
+                            ],
+                        )
+                    ],
+                )
+            )
+            actions.append(
+                reload_on_restart
             )
 
             return [GroupAction(actions)]
